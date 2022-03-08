af0f98b1b8cdf14c8ab3647eeaa546af406add4d
indieka900 <indiekaj@gmail.com>
2022-03-08 21:36:53 +0300

    my app

---
+++ views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render
+from .models import Room
+
+'''
+rooms = [
+    {'id':1, 'name':'Lets learn python!'},
+    {'id':2, 'name':'Design with me'},
+    {'id':3, 'name':'Frontend developers'},
+    {'id':4, 'name':'Web designer'},
+]
+'''
+
+def home(request):
+    rooms = Room.objects.all()
+    context = {'rooms': rooms}
+    return render(request, 'registry/home.html', context)
+
+def room(request, pk):
+    room = Room.objects.get(id=pk)
+    context = {'room':room}
+    return render(request, 'registry/room.html',context)
+
+# Create your views here.
