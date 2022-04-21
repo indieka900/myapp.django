11053b73c0dd6bce6261465fb29b3fd5bd6ed657
indieka900 <indiekaj@gmail.com>
2022-04-21 16:32:02 +0300

    django-app

--- views.py
+++ views.py
@@ -5,6 +5,7 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate,login,logout
 from django.db.models import Q
+from django.contrib.auth.forms import UserCreationForm
 from .models import Room, Topic
 from .forms import RoomForm
 
@@ -17,10 +18,11 @@ rooms = [
 ]
 '''
 def loginPage(request):
-    if request.user.is_authendicated:
+    page = 'login'
+    if request.user.is_authenticated:
         return redirect('home')
     if request.method == 'POST':
-        username = request.POST.get('username')
+        username = request.POST.get('username').lower()
         password = request.POST.get('password')
         try:
             user = User.objects.get(username=username)
@@ -32,12 +34,26 @@ def loginPage(request):
             return redirect('home')
         else:
             messages.error(request, 'Username OR password is incorrect')
-    context = {}
+    context = {'page': page}
     return render(request, 'registry/login_reg.html', context)
 def logoutUser(request):
     logout(request)
     return redirect('home')
+def registerUser(request):
+    form = UserCreationForm()
 
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.username = user.username.lower()
+            user.save()
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request, 'An error occured during registry')
+            
+    return render(request, 'registry/login_reg.html', {'form': form})
 
 def home(request):
     q = request.GET.get('q') if request.GET.get('q') != None else ''
@@ -55,6 +71,7 @@ def home(request):
 
 def room(request, pk):
     room = Room.objects.get(id=pk)
+    messages = room.message_set.all()
     context = {'room':room}
     return render(request, 'registry/room.html',context)
 
